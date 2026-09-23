bd2190c0b35b436223912586aadca9b2b9c589e6
KARTIK SEHRAWAT <kartiksehrawat280@gmail.com>
2026-09-23 21:40:55 +0530

    Add seasonal switch case in problem12.c

---
+++ problem12.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+  int x;
+  scanf("%d",&x);
+  switch(x) {
+    case(3):case(4):case(5):
+    printf("Spring");
+    break;
+    case(6):case(7):case(8):
+    printf("Summer");
+    break;
+    case(9):case(10):case(11):
+    printf("Autum");
+    break;
+    case(12):case(1):case(2):
+    printf("Winter");
+    break;
+  }
+  return 0;
+}
